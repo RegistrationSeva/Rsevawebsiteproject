9453fff2e7302bb7677118934895638942f3f82f
Vishal Chaturvedi <vishalchaubey0011@gmail.com>
2024-08-13 00:15:25 +0530

    Removed Unused Photos

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,13 +1,5 @@
 export default {
   appDev: require("@/assets/images/appdev.jpg"),
-  logoDesign: require("@/assets/images/logodev.jpg"),
-  webDev: require("@/assets/images/webdev.jpg"),
-  digitalMarketing: require("@/assets/images/digitalmarketing.jpg"),
-  uiUxDesign: require("@/assets/images/uiuxdev.jpg"),
   logo: require("@/assets/images/logo.png"),
-  earningApps: require("@/assets/images/earningApps.jpg"),
-  marketResearch: require("@/assets/images/marketResearch.jpg"),
-  publicOpinionPolling: require("@/assets/images/publicOpinionPolling.jpg"),
-  advertisingServices: require("@/assets/images/advertisingServices.jpg"),
   contactUs: require("@/assets/images/contact-us.jpg"),
 };
