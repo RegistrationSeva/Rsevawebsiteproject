7c6aaafaca9e57e9e852359e47b3f4f196c6c596
Vishal Chaturvedi <vishalchaubey0011@gmail.com>
2025-09-27 17:02:11 +0530

    added test-2 data

---
+++ src/app/our-services/annual-compliance-private-limited-company/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam non ipsa
+      ducimus eligendi quia repudiandae rem corrupti quaerat! Tempora provident,
+      odit sit, culpa suscipit nostrum delectus asperiores quis consequatur
+      corporis officiis, assumenda aspernatur pariatur voluptas! Libero,
+      repellendus. Deleniti ullam impedit, nam et aliquam soluta a sed omnis
+      dolorem quisquam cumque tempora, nemo nostrum odit? Consectetur non
+      commodi vel suscipit repudiandae quasi voluptatibus quam reiciendis
+      recusandae, repellat magnam mollitia fugiat impedit ex! Dolor, amet quos
+      ex, deleniti id sequi laudantium ullam inventore nulla deserunt, provident
+      aut at a fugiat autem earum magni. Error magni id ipsam iure sapiente
+      neque? Est provident, consectetur ex necessitatibus delectus pariatur,
+      explicabo distinctio inventore impedit dignissimos recusandae blanditiis
+      eum quas eos cupiditate minus nulla, quasi laboriosam!
+    </div>
+  );
+}
+
+export default Page;
